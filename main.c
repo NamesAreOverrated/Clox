a9ea1c7f68f8aaa33daf742d0128add0a819a58d
NamesAreOverrated <65521809+NamesAreOverrated@users.noreply.github.com>
2026-05-03 13:13:00 +0800

    clox

---
+++ main.c
@@ -0,0 +1,25 @@
+#include "chunk.h"
+#include "common.h"
+#include "debug.h"
+#include "memory.h"
+#include "value.h"
+
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+    printf("Hello World\n");
+
+    Chunk chunk;
+    initChunk(&chunk);
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    disassembleChunk(&chunk, "test chunk");
+
+    freeChunk(&chunk);
+
+    return 0;
+}
